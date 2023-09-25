36e5b9a38eadae096cd4ff44c46b257bece65ce6
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-24 17:59:32 -0700

    tasks 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # To make changes to our configuration file.
 # Just as in the previous config file.
-file_line { 'Turn off passwd auth':
+# file_line { 'Turn off passwd auth':
   ensure => present,
-  line   => '    paswordAuthentication no' ,
+  line   => '    PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
 }
 
-file_line { 'Declare identy file':
+file_line { 'Declare identity file':
   ensure => present,
   line   => '    IdentityFile ~/.ssh/school',
   path   => '/etc/ssh/ssh_config',
