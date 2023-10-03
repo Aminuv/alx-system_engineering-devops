2d13eed4b2fc948d5b5b57220d74c2d1af418789
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-02 18:36:23 -0700

    a shel minesse

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,8 +14,7 @@ exec {'install Nginx':
 
 exec {'add_header':
   provider     => shell,
-  environment  => ["HOST=${hostname}"],
-  command      => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  command      => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
   before       => Exec['restart Nginx'],
 }
 
